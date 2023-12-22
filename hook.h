6da4e8f7d5175edd278b1bf0d518804e972a484c
OverTerm1752 <141875806+OverTerm1752@users.noreply.github.com>
2023-12-22 15:58:58 +0100

    Update hook.h

--- hook.h
+++ hook.h
@@ -6,7 +6,7 @@ bool __forceinline Main()
 	freopen_s(&fp, ("CONOUT$"), ("w"), stdout);
 	CachedData::Base = *(uint64_t*)(__readgsqword(0x60) + 0x10);
 	auto scanneduworld = ResolveRelativeAddress(PScan(("48 89 05 ? ? ? ? 44 38 3D ? ? ? ?")), 7);
-	GetNameByIndex = decltype(GetNameByIndex)(PScan(("48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 33 ED 48 8B FA 48 89 2A 48 89 6A 08 8B")));
+	GetNameByIndex = decltype(GetNameByIndex)(PScan(("48 8B C4 48 89 58 08 48 89 70 18 48 89 78 20 55 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 45 33 ED 48 8B FA 4C 89 2A 4C 89 6A 08 8B 19 E8 ? ? ? ? 48 8B D0 8B CB 48 C1 E9 10 0F B7 C3 44 8D 3C 00 4C 03 7C CA ? 66 41 83 3F ? 0F 82 ? ? ? ? 41 0F B7 07 C1 E8 06 85 C0 44 89 6F 08 8D 50 01 41 0F 44 D5 3B 57 0C 0F 8F ? ? ? ?")));
 	LineSightOfTo = decltype(LineSightOfTo)(PScan(("48 8B C4 48 89 58 20 55 56 57 41 54 41 56 48 8D 68")));
 	GetBoneMatrix = PScan(("E8 ? ? ? ? 0F 10 40 68"));
 	GetBoneMatrix = RVA(GetBoneMatrix, 5);
